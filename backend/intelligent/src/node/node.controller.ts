702b540ef36b2b1ea0d79dc3a8dd4e5188bd9219
GODPARK <rootjh937@gmail.com>
2021-01-06 22:50:30 +0900

    node controller 고도화

--- backend/intelligent/src/node/node.controller.ts
+++ backend/intelligent/src/node/node.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Body, Controller, Get, HttpStatus, Post, Query, Res } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, HttpStatus, Param, Patch, Post, Put, Query, Res } from '@nestjs/common';
 import { NodeService } from './node.service';
 import { NodeDto } from '../dto/node.dto';
 import { Node } from '../schemas/node.schema'
@@ -7,18 +7,32 @@ import { Node } from '../schemas/node.schema'
 export class NodeController {
     constructor(private readonly nodeService: NodeService) {}
     
-    @Get()
-    getNode(@Query('id') id: string ) {
-        return this.nodeService.findOne(id)
-    }
-
     @Get('all')
     getNodeALL(): Promise<Node[]> {
         return this.nodeService.findAll()
+    }
+
+    @Get()
+    getNodeByBody(@Body() idDto: { id: string }): Promise<Node> {
+        return this.nodeService.findOne(idDto.id)
+    }
+
+    @Get('/:id')
+    getNodeByParam(@Param('id') id: string ) {
+        return this.nodeService.findOne(id)
     }
 
     @Post()
     createNode(@Body() nodeDto: NodeDto): Promise<Node> {
         return this.nodeService.create(nodeDto)
     }
+
+    // @Patch()
+    // updateNode(@Body() NodeDto: NodeDto): Promise<Node> {
+        
+    // }
+    // @Delete()
+    // deleteNode(@Body() idDto: { id: string }): Promise<Node> {
+    //     return this.nodeService
+    // }
 }
